b47a890fa396fd2f5ee467afe35d9688968edd6f
Pablo Sirera <psirera4@gmail.com>
2021-04-08 22:44:11 +0200

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,26 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="cam-frame"></div>
+    <h3 class="chat-title">-- CHAT --</h3>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
-  name: "Home",
-  components: {
-    HelloWorld,
-  },
-};
+  name: 'Home',
+}
 </script>
+
+<style lang="scss" scoped>
+.cam-frame {
+  border: 5px solid rgb(67, 93, 76);
+  border-radius: 5px;
+  width: 250px;
+  height: 150px;
+}
+
+.chat-title {
+  width: 250px;
+  text-align: center;
+}
+</style>
