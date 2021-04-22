d039c35fb62c48bc02a58a3b6d7673235b81c7bf
Pablo Sirera <psirera4@gmail.com>
2021-04-22 22:51:17 +0200

    feat: create new netlify function

---
+++ netlify/functions/twitch-follows/twitch-follows.js
@@ -0,0 +1,24 @@
+exports.handler = async (event) => {
+  const { headers = [] } = event
+
+  const type = headers['twitch-eventsub-message-type'] || 'no type'
+  const eventType = headers['twitch-eventsub-follow-type']
+
+  if (type !== 'notification' || eventType !== 'channel.follow') {
+    return { statusCode: 200, body: '' }
+  }
+
+  console.log({ headers: event.headers, body: event.body })
+
+  // at this point you know you have a valid subscription
+  // want to do some magic? put it here!
+  const { event: twitchEvent } = JSON.parse(event.body)
+  const user = twitchEvent.user_name
+
+  console.log(`${user} acaba de unirse!`)
+
+  return {
+    statusCode: 200,
+    body: '',
+  }
+}
