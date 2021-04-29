ba8fb83772d692978469cd384c419b730c1320e7
Pablo Sirera <psirera4@gmail.com>
2021-04-29 23:23:26 +0200

    use require

--- netlify/functions/twitch-follows/twitch-follows.js
+++ netlify/functions/twitch-follows/twitch-follows.js
@@ -1,13 +1,15 @@
-import { emitter } from '../../../src/utils/emitter'
+const { emitter } = require('../../../src/utils/emitter')
 
 exports.handler = async (event) => {
   const { headers = [] } = event
   // const type = headers['twitch-eventsub-message-type'] || 'no type'
   const eventType = headers['twitch-eventsub-subscription-type']
 
+  console.log(eventType)
+
   if (eventType === 'channel.follow') {
     console.log('entro en el if')
-    emitter.emit('new-follow')
+    emitter.$emit('new-follow')
     return { statusCode: 200, body: '' }
   }
 
