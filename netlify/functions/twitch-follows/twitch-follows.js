8f4770e20d923309a09344744e9e02ddd7ccbcc8
Pablo Sirera <psirera4@gmail.com>
2021-04-29 23:52:03 +0200

    get response post

--- netlify/functions/twitch-follows/twitch-follows.js
+++ netlify/functions/twitch-follows/twitch-follows.js
@@ -1,21 +1,21 @@
-import { inject } from 'vue'
+// import { inject } from 'vue'
 
 exports.handler = (event) => {
   const { headers = [] } = event
   // const type = headers['twitch-eventsub-message-type'] || 'no type'
   const eventType = headers['twitch-eventsub-subscription-type']
-  const emitter = inject('emitter')
+  // const emitter = inject('emitter')
 
   console.log(eventType)
 
   if (eventType === 'channel.follow') {
     console.log('entro en el if')
-    emitter.emit('new-follow')
-    return { statusCode: 200, body: '' }
+    // emitter.emit('new-follow')
+    return { statusCode: 200, body: 'asd' }
   }
 
   return {
     statusCode: 200,
-    body: '',
+    body: 'asd2',
   }
 }
