071534c2433e61c812d13e0425c370da3ccb23a5
Pablo Sirera <psirera4@gmail.com>
2021-09-10 19:42:26 +0200

    fix: use hash router mode

--- src/router/index.js
+++ src/router/index.js
@@ -1,4 +1,4 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import Primary from '@/views/Primary.vue'
 
 const routes = [
@@ -15,7 +15,7 @@ const routes = [
 ]
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHashHistory(),
   routes,
 })
 
